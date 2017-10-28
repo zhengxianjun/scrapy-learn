32286d9d5c126daf643cd6b1788cb88da4cdd41a
Zhengxianjun <263661353@qq.com>
2017-10-28 21:18:41 +0800

    tutorial project

---
+++ tutorial/tutorial/spiders/dmoz_spyder.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    # def parse(self, response):
+    #     filename = response.url.split("/")[-2]
+    #     with open(filename,'wb') as f :
+    #         f.write(response.body)
+    def parse(self, response):
+        for sel in response.xpath('//ul/ui'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+        yield item
+        
