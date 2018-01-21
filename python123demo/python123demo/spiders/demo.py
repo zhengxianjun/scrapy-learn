c8bd39a2a23213aa43f063b0a10827f7cce68662
ZHENGXIANJUN <263661353@qq.com>
2018-01-21 17:48:14 +0800

    add python123demo

---
+++ python123demo/python123demo/spiders/demo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class DemoSpider(scrapy.Spider):
+    name = 'demo'
+#    allowed_domains = ['python123.io']
+#    start_urls = ['http://python123.io/ws/demo.html']
+    
+    # demo.py 完整版代码
+    def start_requests(self):
+        urls = ['http://python123.io/ws/demo.html']  
+        for url in urls:
+            yield scrapy.Request(url=url,callback=self.parse)
+
+    def parse(self, response):
+        fname = response.url.split('/')[-1]
+        with open(fname,'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s.'% fname)
